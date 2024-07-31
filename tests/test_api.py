2dd6b4252e7f45be01de1c4f37cee06993513ee8
RobertoFahmy <roberto.fahmy@gmail.com>
2024-07-30 22:36:09 -0300

    coments

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,1 +1,7 @@
-response.json()
+import pytest
+
+@pytest.mark.order(1)
+def test_get_aluno(api_client):
+    response = api_client.get("/aluno/")
+    assert response.status_code == 200
+    assert response.json() == "Olá alunos"
